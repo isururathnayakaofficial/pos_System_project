6807a9478aacd9c33882a2d8f25454a15ce43d82
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-02-11 11:51:54 +0530

    10th commit-item saved successfully with frontEnd

--- Front_End/js/item.js
+++ Front_End/js/item.js
@@ -1,1 +1,29 @@
-$('#')
+$('#saveItem').click(function (){
+    var item={
+        Iid:$('#itemId').val(),
+        IName:$('#itemName').val(),
+        IQuantity:$('#itemQty').val(),
+        IPrice:$('#itemPrice').val()
+    };
+
+    if (!item.IName || !item.IPrice || !item.IPrice){
+        alert("fill the all fields");
+        return;
+    }
+    $.ajax({
+        url:'http://localhost:8080/api/v2/save_item',
+        type:'POST',
+        contentType:'application/json',
+        data:JSON.stringify(item),
+        success:function (){
+            alert("Item saved successfully");
+
+            $('#itemName').val(''),
+            $('#itemQty').val(''),
+                $('#itemPrice').val('')
+        },
+        error:function (err){
+            alert("Error saving customer!")
+        }
+    })
+})
